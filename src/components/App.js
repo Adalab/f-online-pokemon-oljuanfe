b513f8a4448cd3b027efe32b330fb501a122795b
oljuanfe <ojuanferrero@gmail.com>
2018-10-06 22:52:48 +0200

    Get fetch for one pokemon, wip for 25 pokemons

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,38 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      
-    }
+      data: [],
+    };
+    this.askForPokemons = this.askForPokemons.bind(this);
   }
+
+  componentDidMount () {
+    for(let i = 0; i < 25; i++ ){
+      this.askForPokemons(i);
+    }
+    
+  }
+
+  askForPokemons (i) {
+    console.log('pidiendooo');
+    let pokemonNumber = i + 1;
+      console.log('pokemonNumber', pokemonNumber);
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber}/`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      console.log('json',json);
+
+      return this.setState({...this.state.data,data: json});
+    })
+  }
+
+
   render() {
+    console.log('state', this.state);
     return (
-      <div className="App">
+      <div>
         <SearchBox/>
         <PokemonList/>
       </div>
