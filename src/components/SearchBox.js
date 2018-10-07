1969d5ce91a18fbc8d2c0aaec8ad127f872cd697
oljuanfe <ojuanferrero@gmail.com>
2018-10-07 20:08:04 +0200

    Get filter pokemon by id and show them

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,10 +2,22 @@ import React, { Component, Fragment } from 'react';
 
 class SearchBox extends Component {
   render() {
+    const {
+      handleFilterPokemon,
+      valueOnSearchBox
+    } = this.props;
+    console.log('value', valueOnSearchBox);
     return (
       <Fragment>
         <label htmlFor="searchPokemon"></label>
-        <input type="text" name="searchPokemon" id="searchPokemon" placeholder="Filtra pokemons por nombre..."/>
+        <input 
+          type="text" 
+          name="searchPokemon" 
+          id="searchPokemon" 
+          placeholder="Filtra pokemons por nombre..."
+          value={valueOnSearchBox}
+          onChange={handleFilterPokemon}
+        />
       </Fragment>
       
     );
