ff10e8137194dbd1f9e848c9e23065d43f656804
oljuanfe <ojuanferrero@gmail.com>
2018-10-13 21:43:16 +0200

    Add some properties for detail view, whether there is info or not

--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -3,12 +3,45 @@ import { Link} from 'react-router-dom';
 
 class PokemonDetail extends Component {
   render() {
-    // console.log('PokemonDetails',this.props);
-    // console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+    console.log('PokemonDetails',this.props);
+    const{
+      match,
+      pokemonData,
+    } = this.props;
+    const {params} = match;
+    
+    const pokemonId = parseInt(params.id);
+    // console.log('id number?',characterId);
+    let pokemonChosen = pokemonData.filter((pokemon) => {
+      return pokemon.id === pokemonId;
+    });
+    console.log('pokemonchosen',pokemonChosen);
+    let pokemonInfo = pokemonChosen[0];
+    console.log('pokemonInfo', pokemonInfo);
+    
     return (
       <div >
-        Holaaaa!! que tal
-        <Link to="/" className="btn-back" >Volver al listado</Link>
+        <div>
+          <img src="" alt=""/>
+          <p>{pokemonInfo!==undefined?pokemonInfo.name:'No data'}</p>
+          <ul>
+            <li>
+              Altura: {pokemonInfo!==undefined?pokemonInfo.height:'No data'}
+            </li>
+            <li>
+              Peso: {pokemonInfo!==undefined?pokemonInfo.weight:'No data'}
+            </li>
+            <li>
+              Habilidades: 
+            </li>
+          </ul>
+        </div>
+        <Link 
+          to="/" 
+          className="btn-back" 
+        >
+          Volver al listado
+        </Link>
       </div>
     );
   }
